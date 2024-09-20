03f9194b13bf5b32c05aaa48ea8128ae268b5967
aaronw143 <aaronw1975@icloud.com>
2024-09-20 08:26:52 -0700

    update shipping_accounts.py

--- shipping_accounts.py
+++ shipping_accounts.py
@@ -1,5 +1,5 @@
 # List of accounts
-users = ["aaronw", "americac", "vincentg", "josephf", "zhu"]
+users = ["aaronw", "americac", "vincentg", "josephf", "mrzhu"]
 
 # Display welcome message and get username
 print("Welcome to the Shipping Accounts Portal!")
@@ -9,31 +9,34 @@ if username in users:
     # Show shipping prices
     print(f"\nWelcome back to your account, {username}!")
     print("These are the current shipping prices:")
-    print("")
-    print("Shipping 0-99 items: $5.10/ea")
-    print("Shipping 100-499 items: $5.00/ea")
-    print("Shipping 500-999 items: $4.95/ea")
-    print("Shipping 1,000+ items: $4.80/ea")
-    print("")
+    print("\n[Amount of Items]\t\t\t[Price]")
+    print("Shipping 0-99 items:\t\t$5.10/item")
+    print("Shipping 100-499 items:\t\t$5.00/item")
+    print("Shipping 500-999 items:\t\t$4.95/item")
+    print("Shipping 1,000+ items:\t\t$4.80/item")
 
     # Get number of orders from the user
     # If the order string is numeric, continue with the program
-    items = input("How many items do you want to ship? ")
+    items = input("\nHow many items do you want to ship? ")
     if items.isnumeric():
         items = int(items)
 
-        # Depending on the amount of orders, display the correct prices
+        # Depending on the amount of orders, set the correct cost per item
         if items == 0:
             input("You must ship more than 0 items. ")
             raise SystemExit
-        elif 0 < items <= 99:
-            print(f"To ship {items} items, it will cost you ${items*5.10:.2f} at $5.10 per item.")
-        elif 100 <= items <= 499:
-            print(f"To ship {items} items, it will cost you ${items*5.00:.2f} at $5.00 per item.")
-        elif 500 <= items <= 999:
-            print(f"To ship {items} items, it will cost you ${items*4.95:.2f} at $4.95 per item.")
+        elif items < 100:
+            cost = 5.10
+        elif items < 500:
+            cost = 5.00
+        elif items < 1000:
+            cost = 4.95
         else:
-            print(f"To ship {items} items, it will cost you ${items*4.80:.2f} at $4.80 per item.")
+            cost = 4.80
+
+        # Calculate and display bill
+        bill = round(items * cost, 2)
+        print(f"To ship {items} items, it will cost you ${bill:.2f} at ${cost:.2f} per item.")
 
         # Confirm or cancel the order
         if input("\nWould you like to place this order (y/n)? ").lower().startswith("y"):
