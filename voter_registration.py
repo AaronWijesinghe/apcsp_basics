262902615e90c67471f700e0f0b99dc03d61b8c3
aaronw143 <aaronw1975@icloud.com>
2024-09-23 08:34:37 -0700

    update shipping_accounts.py

---
+++ voter_registration.py
@@ -0,0 +1,47 @@
+# ANSI codes to be used throughout the program
+bold = "\033[1m"
+underline = "\033[4m"
+end = "\033[0m"
+
+# Display welcome message
+print("Welcome to the Voter Registration App!")
+
+# Ask for name/age and set list of parties
+name = input("\nPlease enter your name: ").title()
+age = input("Please enter your age: ")
+parties = ["republican", "democratic", "independent", "libertarian", "green"]
+
+# If the user's age isn't a number, exit
+if not age.isnumeric():
+    print("\nYou must enter a number for your age.")
+    raise SystemExit
+else:
+    age = int(age)
+
+if age >= 18:
+    # If age is over 18, display parties and congratulate user
+    print(f"\nCongratulations {name}! You are old enough to register to vote.")
+    print(f"\n{underline}Here is a list of political parties to join:{end}")
+    for _ in parties:
+        print(f"\t• {_.title()}")
+
+    # Ask user what party they want to vote for
+    party = input("\nWhat party would you like to join? ").lower()
+    print("")
+    if party in parties:
+        print(f"Congratulations {name}! You have joined the {party.title()} party!")
+    else:
+        print(f"{name}, this party does not exist.")
+        raise SystemExit
+
+    # Check if the party is major/minor/independent
+    if party in ["republican", "democratic"]:
+        print("That is a major party!")
+    elif party in ["libertarian", "green"]:
+        print("That is not a major party.")
+    else:
+        print("You are an independent person!")
+
+else:
+    # If the user is under 18 years old, exit the program
+    print(f"\nYou are not old enough to vote. {underline}You must be 18+ to vote.{end}")
